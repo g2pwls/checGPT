a6602fc65ae87f0522efce8012c77ef677d4681a
Hyejin <yhjyhw1004@naver.com>
2025-05-22 17:15:32 +0900

    북리스트 완성

--- django/books/views.py
+++ django/books/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 from .models import Book, Category
 from .serializers import BookSerializer, CategorySerializer
 from django.db.models import Q 
