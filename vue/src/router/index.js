5de33f87044b6c8d224ac4b20d29adb1f3fa75aa
jihyun <annn0402@gmail.com>
2025-05-22 16:00:26 +0900

    signupview 생성

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignUpView from '@/views/SignUpView.vue'
+import MyPageView from '@/views/MyPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +12,14 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/signup',
+      name: 'SignUpView',
+      component: SignUpView,
+    },
+    {
+      path: '/mypage',
+      name: 'MyPageView',
+      component: MyPageView,
     },
   ],
 })
